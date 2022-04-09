687cad3cf1c6f442e7283e38529e7df48eddd2ce
Space Map Server <spacemap@server>
2022-04-09 04:54:14 +0000

    use: eslint with prettier

--- src/routes/contact.route.js
+++ src/routes/contact.route.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const ContactController = require('../controllers/contact.controller');
 const wrapper = require('../lib/request-handler');
 
-
-router.post("/", wrapper(ContactController.createModel));
-router.get("/", wrapper(ContactController.readModels));
-router.get("/:id", wrapper(ContactController.readModel));
-router.delete("/:id", wrapper(ContactController.deleteModel));
-
+router.post('/', wrapper(ContactController.createModel));
+router.get('/', wrapper(ContactController.readModels));
+router.get('/:id', wrapper(ContactController.readModel));
+router.delete('/:id', wrapper(ContactController.deleteModel));
 
 module.exports = router;
